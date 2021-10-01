ef649a8144e5393d1ff33f2e51c39dc81fa8a2c3
Aulia rahman <82009950+Rhmn9@users.noreply.github.com>
2021-10-01 15:16:12 +0700

    iya in aja lah

--- message/group.js
+++ message/group.js
@@ -10,31 +10,31 @@ const { getBuffer, sleep } = require("../lib/myfunc");
 let setting = JSON.parse(fs.readFileSync('./config.json'));
 let { botName } = setting
 
-module.exports = async(xinz, anj, welcome, left) => {
+module.exports = async(aulia, anj, welcome, left) => {
     const isWelcome = welcome.includes(anj.jid)
     const isLeft = left.includes(anj.jid)
-    const mdata = await xinz.groupMetadata(anj.jid)
+    const mdata = await aulia.groupMetadata(anj.jid)
     const groupName = mdata.subject
 
     if (anj.action === 'add'){
         if (anj.participants[0] === xinz.user.jid){
             await sleep(5000)
-            xinz.updatePresence(anj.jid, Presence.composing)
-            xinz.sendMessage(anj.jid, `Hai aku ${botName}, Terima kasih telah memasuki bot ke dalam group anda , silahkan ketik #menu`, MessageType.text)
+            aulia.updatePresence(anj.jid, Presence.composing)
+            aulia.sendMessage(anj.jid, `hai aku ${botName}, Terima kasih telah memasuki bot ke dalam group anda , silahkan ketik #menu`, MessageType.text)
         } else if (isWelcome){
            try {
-                var pic = await xinz.getProfilePicture(anj.participants[0])
+                var pic = await aulia.getProfilePicture(anj.participants[0])
             } catch {
                 var pic = 'https://i.ibb.co/Tq7d7TZ/age-hananta-495-photo.png'
             }
-            xinz.sendMessage(anj.jid, await getBuffer(pic), MessageType.image, {caption: `Hai @${anj.participants[0].split("@")[0]}, selamat datang di ${groupName}\n\n Baru Masuk Wajib Intro Ya Kak\n\nINTRO/DIKICK:\n\n\nNAMA :\n\nKELAS :\n\nUMUR :\n\nASKOT :\n\n\nWAJIB DI ISI!`, contextInfo: {"mentionedJid": [anj.participants[0]]}})
+            aulia.sendMessage(anj.jid, await getBuffer(pic), MessageType.image, {caption: `hai @${anj.participants[0].split("@")[0]}, selamat datang di ${groupName}\n\nbaru masuk wajib intro gk intro di kick-!!`, contextInfo: {"mentionedJid": [anj.participants[0]]}})
         }
     } else if (anj.action === 'remove' && isLeft){
         try {
-                var pic = await xinz.getProfilePicture(anj.participants[0])
+                var pic = await aulia.getProfilePicture(anj.participants[0])
             } catch {
                 var pic = 'https://i.ibb.co/Tq7d7TZ/age-hananta-495-photo.png'
             }
-        xinz.sendMessage(anj.jid, await getBuffer(pic), MessageType.image, {caption: `sayonaraa👋... @${anj.participants[0].split("@")[0]} jangan bawa makanan cuy:v`, contextInfo: {"mentionedJid": [anj.participants[0]]}})
+        aulia.sendMessage(anj.jid, await getBuffer(pic), MessageType.image, {caption: `sayonaraa👋... @${anj.participants[0].split("@")[0]} jangan bawa makanan cuy:v`, contextInfo: {"mentionedJid": [anj.participants[0]]}})
     }
 }
